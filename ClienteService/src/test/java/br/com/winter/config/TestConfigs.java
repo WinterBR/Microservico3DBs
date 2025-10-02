9b7247ba2534acfa26f0ec7d87ebe472c90bd57c
Eduardo Winter <eduardowinterbr@gmail.com>
2025-10-02 17:55:21 -0300

    docker compose

--- ClienteService/src/test/java/br/com/winter/config/TestConfigs.java
+++
@@ -1,9 +0,0 @@
-package br.com.winter.config;
-
-public class TestConfigs {
-
-    public static final int SERVER_PORT = 9082;
-    public static final String CONTENT_TYPE_JSON = "application/json";
-    public static final String HEADER_PARAM_ORIGIN = "Origin";
-    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
-}
